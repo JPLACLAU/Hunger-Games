a47bac8a7a1160dc8aebeac56dd5cab95756636b
jplaclau <laclaujeanpaul@gmail.com>
2023-01-26 13:19:12 -0300

    z

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,35 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require("dotenv").config()
+require("@nomiclabs/hardhat-etherscan")
 
-/** @type import('hardhat/config').HardhatUserConfig */
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || ""
+const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL || ""
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ""
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ""
+
 module.exports = {
-  solidity: "0.8.17",
-};
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {},
+        goerli: {
+            url: GOERLI_RPC_URL,
+            accounts: [PRIVATE_KEY],
+            chainId: 5,
+        },
+        localhost: {
+            url: "http://localhost:8545",
+            chainId: 31337,
+        },
+    },
+    solidity: "0.8.13",
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: true,
+        currency: "USD",
+        outputFile: "gas-report.txt",
+        noColors: true,
+        coinmarketcap: COINMARKETCAP_API_KEY,
+    },
+}
